29d16298128f0d30c76c2f194daa0c6299d53a7c
rcviale <77018783+rcviale@users.noreply.github.com>
2023-12-11 13:49:51 +0100

    Eliminate selection at the end.

--- R/lrets.R
+++ R/lrets.R
@@ -2,6 +2,5 @@ lrets <- function(.data, .ticker_col, .px_col){
   .data |> 
     dplyr::group_by({{.ticker_col}}) |> 
     dplyr::mutate(ret = log({{.px_col}} / dplyr::lag({{.px_col}}, n = 1L))) |> 
-    dplyr::ungroup() |> 
-    dplyr::select(-{{.px_col}})
+    dplyr::ungroup()
 }
